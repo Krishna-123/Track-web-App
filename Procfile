197f6e8cfaf5a52e44b3dc27473acdab911d5393
krishna-123 <krishna11agarwal@gmail.com>
2017-08-25 01:20:59 +0530

    try to make it better

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node TCPServer.js
+web: node index.js
---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+var port = process.env.PORT || 3000;
+
+app.post('/', function(req, res){
+    staTus = req.body.data;
+
+ console.log(staTus);   
+ console.log('Post req received ');
+
+ res.status(200);
+ res.end();
+});
+
+var server = app.listen(port,function () {
+  console.log("Example app listening at ")
+});
+
