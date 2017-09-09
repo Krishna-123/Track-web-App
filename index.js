90fcc09044765d86f9bebc9cc38a254e0ce24819
Krishna-123 <krishna11agarwal@gmail.com>
2017-09-09 23:06:40 +0530

    initial commit

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-
+/*app.use(bodyParser.raw({
+    type: "text/html"
+}));
+*/
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
@@ -15,15 +18,27 @@ app.get('/', function(req, res){
               
 	 res.status(200);
 	 res.end();
-	
 });
 
 app.post('/', function(req, res){
-    var staTus = JSON.stringify(req.body);
+   // var staTus = JSON.stringify(req.body);
+   //var locObj =  JSON.parse(req.body);
+   // var status = JSON.stringify(eval("(" + req.body + ")"));
 
- console.log(staTus);   
+    // var status = JSON.stringify(req.body);
+    // console.log("Request Body Obj: "+status );
+
+	// console.log(staTus);   
+   var obj = JSON.stringify(req.body) ;
+   var obj1 = JSON.parse(obj) ;
+
+ console.log("Post Request: "+  obj);
+ console.log("Post Request Param: "+  obj1.Lat);
+
  console.log('Post req received ');
 
+ var staTus = "done";
+ 
  res.status(200);
  res.end(staTus);
 });
